61154f696a1e042ba87d045d7b16a8bb9e91c9e4
annegleichmann <anne.gleichmann@web.de>
2025-01-04 12:56:27 +0100

    added GitHub links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/annegleichmann" target="_blank">
+          Anne Gleichmann
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/annegleichmann/shecodes-react-weather-app"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
